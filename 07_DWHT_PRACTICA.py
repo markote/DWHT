90be119e807abe8e8d1c2797972a3e66d7d49eaa
Marco <marco.terral@hotmail.com>
2019-06-05 14:39:14 +0200

    completamente funcional

--- 07_DWHT_PRACTICA.py
+++ 07_DWHT_PRACTICA.py
@@ -5,8 +5,8 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-
-
+import math
+from copy import copy, deepcopy
 """
 Implementar una funcion H_WH(N) que devuelva la matriz NxN asociada a la transformación de Walsh-Hadamard
 
@@ -37,23 +37,21 @@ def fusiondevec(m1,m2):
     return mres
 
 def compose(matrix):
-    mat1=matrix
-    mat2=matrix
-    mat3=matrix
-    mat4=multmat(-1,matrix)
+    mat1=[row[:] for row in matrix]
+    mat2=[row[:] for row in matrix]
+    mat3=[row[:] for row in matrix]
+    mat4=multmat(-1,[row[:] for row in matrix])
     m12=fusiondevec(mat1,mat2)
-    print("m12:",str(m12))
+    #print("m12:",str(m12))
     m34=fusiondevec(mat3,mat4)
-    print("m34:",str(m34))
+    #print("m34:",str(m34))
     return m12+m34
 
-print(compose([[1,1],[1,-1]]))
-
 def defmataux(N):
-    if N==1 :
-        return [[1,1],[1,-1]]
+    if N<2 :
+        return [[1]]
     else:
-        mataux=defmataux(N-1)
+        mataux=defmataux(N-N/2)
         return compose(mataux)
 
 def H_WH(N):
@@ -64,13 +62,34 @@ def H_WH(N):
     scores=sorted(scores)#calculo de la diferencias de cada vector para ordenar las filas
     result=[]
     for s in scores:
-        result.append(mataux[s[1]])#reordenacion de la matrix auxiliar
-    return multmat(1/sqrt(N),result)
+        result.append(maux[s[1]])#reordenacion de la matrix auxiliar
+    return multmat(1/math.sqrt(N),result)
 
         
+print(H_WH(4))
+
+def dwht_bloque(p):
+    HWH=H_WH(len(p))
+    return np.tensordot(np.tensordot(HWH, p, axes=([1], [0])), HWH, axes=([1], [0]))
 
 
+def idwht_bloque(p):
+    HWH=H_WH(len(p))
+    return np.tensordot(np.tensordot(HWH, p, axes=([1], [0])), HWH, axes=([1], [0]))
 
+print(dwht_bloque(
+            [[217,   8, 248, 199],
+             [215, 189, 242,  10],
+             [200,  65, 191,  92],
+             [174, 239, 237, 118]]
+            ))
+
+print(idwht_bloque(
+            [[ 661,   -7.5, -48.5, 201],
+             [   3,  -27.5,  25.5,  57],
+             [  59,  -74.5,  36.5, -45],
+             [ -51, -112.5, 146.5,  45]]
+            ))
 """
 Implementar la DWHT (Discrete Walsh-Hadamard Transform) y su inversa
 para bloques NxN
@@ -92,14 +111,28 @@ dwht_bloque(
              [ -51, -112.5, 146.5,  45]]
 
 """
-#def dwht_bloque(p):
+def crearBloque(x=0, y=0, d=8):
+    m = np.zeros((d,d))
+    m[x][y] = 1;
+    return m
+    
+def bloquesBase(N=4):
+    for i in range(N * N):
+        x = int(i / N)
+        y = i % N
+        
+        m = crearBloque(x, y, N)
+        img = dwht_bloque(m)
+        
+        print(img)
+        plt.imshow(img) 
+        plt.xticks([])
+        plt.yticks([])
+        plt.show() 
+        print('#####################')
 
-
-
-
-#def idwht_bloque(p):
-
-
+print("--------")
+bloquesBase(16)# 4, 8 o 16
 """
 Reproducir los bloques base de la transformación para los casos N=4,8,16
 Ver imágenes adjuntas
